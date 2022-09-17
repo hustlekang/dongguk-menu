738e566594159f8498e880d74a81561abbc51de4
hustlekang <rkdgusrn9688@gmail.com>
2022-09-17 17:54:12 +0900

    Fix: netlify proxy 설정

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -3,7 +3,9 @@ import wrapPromise from "./wrapPromise";
 
 // http 요청을 통해 html 문서를 받아서 string 타입으로 반환
 const fetchHtml = async (sday) => {
-  const query = "/menu.html?code=5&sday=" + sday;
+  //netlify proxy오류 해결
+  const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
+  const query = PROXY + "/menu.html?code=5&sday=" + sday;
   try {
     const res = await fetch(query);
     return res.text();
@@ -19,13 +21,16 @@ const getMenu = async (sday) => {
 };
 
 const fetchInitHtml = () => {
+  //netlify proxy오류 해결
+  const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
+
   const yymmdd = new Date()
     .toLocaleDateString()
     .match(/[0-9]+/g)
     .map((date) => (date.length === 1 ? 0 + date : date))
     .join("-");
   const sday = new Date(yymmdd).getTime() / 1000 - 32400;
-  const query = "/menu.html?code=5&sday=" + sday;
+  const query = PROXY + "/menu.html?code=5&sday=" + sday;
   const promise = fetch(query).then((res) => res.text());
 
   return wrapPromise(promise);
