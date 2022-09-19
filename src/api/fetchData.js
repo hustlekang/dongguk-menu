fe113648b61f66a8db20d9545ee555ff2422799d
hustlekang <rkdgusrn9688@gmail.com>
2022-09-19 16:25:51 +0900

    Fix: api 주소 수정

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -3,7 +3,7 @@ import wrapPromise from "./wrapPromise";
 
 // http 요청을 통해 html 문서를 받아서 string 타입으로 반환
 const fetchHtml = async (sday) => {
-  const query = "/menu.html?code=5&sday=" + sday;
+  const query = "/api/menu.html?code=5&sday=" + sday;
   try {
     const res = await fetch(query);
     return res.text();
@@ -25,7 +25,7 @@ const fetchInitHtml = () => {
     .map((date) => (date.length === 1 ? 0 + date : date))
     .join("-");
   const sday = new Date(yymmdd).getTime() / 1000 - 32400;
-  const query = "/menu.html?code=5&sday=" + sday;
+  const query = "/api/menu.html?code=5&sday=" + sday;
   const promise = fetch(query).then((res) => res.text());
 
   return wrapPromise(promise);
